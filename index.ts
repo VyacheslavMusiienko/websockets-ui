842c13797cc56258c26e7c98d44c1e9ebea9213a
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-07-07 14:42:21 +0300

    feat: add settings for reader

--- index.ts
+++ index.ts
@@ -4,3 +4,8 @@ const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+process.on('SIGINT', async () => {
+  httpServer.close();
+  process.exit(0);
+});
